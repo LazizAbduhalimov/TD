2e1b8c142f303b0e0ad9af8e4582195197475315
LazizGameDeveloper <lazizabduhalimov35@gmail.com>
2022-08-27 14:03:17 +0500

    second commit
    NavMesh was added
    map was expanded

---
+++ TD/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        agent.SetDestination(target.position);
+    }
+
+}
